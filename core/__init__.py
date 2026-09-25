93e80c2a8e6b4de7f9c87a79637fa3ac206e3deb
Charlie <56288592+CharlieG42@users.noreply.github.com>
2026-09-25 08:45:54 +0200

    refonte v2: suppression de core/__init__.py (ancienne application Python/QML, sauvegarde faite)

--- core/__init__.py
+++
@@ -1,7 +0,0 @@
-# This Python file uses the following encoding: utf-8
-from .hydraulics import CalcJ, CalcNPSHd
-from .pump import Pump
-from .installation import Installation
-from .graph import Graph
-
-__all__ = ["CalcJ", "CalcNPSHd", "Pump", "Installation", "Graph"]

f79af82dfc6091843d8677747b83efe2d8d8895e
Charlie <56288592+CharlieG42@users.noreply.github.com>
2026-09-25 08:45:34 +0200

    refonte v2: suppression de backend/__init__.py (ancienne application Python/QML, sauvegarde faite)

--- backend/__init__.py
+++
@@ -1,6 +0,0 @@
-# This Python file uses the following encoding: utf-8
-from .app_backend import Backend
-from .maint_backend import MaintenanceBackend
-from .biz_backend import BizBackend
-
-__all__ = ["Backend", "MaintenanceBackend", "BizBackend"]
